a02eac80cc2cd886c795d568ece5b1340d906b08
ntrent1 <trentnr@etsu.edu>
2026-02-12 14:22:10 -0500

    Broken Enemy Movement

--- Godot/appalachian-trail-game/scenes/entities/enemies/enemy.gd
+++ Godot/appalachian-trail-game/scenes/entities/enemies/enemy.gd
@@ -3,6 +3,14 @@ extends CharacterBody2D
 @export_category("Stats")
 @export var hitpoints:int = 180
 
+var player_position
+var target_position
+@onready var player = $"../Player"
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_playback: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
+
+func _ready() -> void:
+	animation_tree.set_active(true)
 
 func  take_damage(damage_taken: int) -> void:
 	hitpoints -= damage_taken
@@ -12,3 +20,13 @@ func  take_damage(damage_taken: int) -> void:
 
 func death() -> void:
 	queue_free()
+	
+func _physics_process(delta):
+	player_position = player.position
+	target_position = (player_position - position).normalized()
+	
+	if position.distance_to(player_position) > 0:
+		#position.move_toward(target_position,100*delta)
+		var direction = target_position
+		velocity = direction * 150
+		move_and_slide()
