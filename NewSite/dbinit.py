404342b5e8e1af5eb0e232acc98cf89461780e0c
LoonchBoox <adalino900@hotmail.com>
2026-04-16 13:33:26 -0400

    Added Password Hashing

--- NewSite/dbinit.py
+++ NewSite/dbinit.py
@@ -1,6 +1,11 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base 
 from sqlalchemy.orm import sessionmaker
+import hashlib
+
+
+def _hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
 engine = create_engine('sqlite:///apptrail.db', echo=True)
 
@@ -32,8 +37,8 @@ session = Session()
 
 # add initial users
 if not session.query(User).first():  # Check if the table is empty before adding users
-    user1 = User(username='spongebob', password='spongebob123', score=0)
-    user2 = User(username='patrick', password='patrick123', score=20)
+    user1 = User(username='spongebob', password=_hash_password('spongebob123'), score=0)
+    user2 = User(username='patrick', password=_hash_password('patrick123'), score=20)
     session.add_all([user1, user2])
     session.commit()
 
