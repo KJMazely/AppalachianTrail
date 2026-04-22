690cf9eaab35a7586df460a2b57c587939780ad9
LoonchBoox <adalino900@hotmail.com>
2026-04-21 20:03:57 -0400

    Updated game version, fixed spelling error

--- NewSite/static/GameFiles/apptrail.service.worker.js
+++ NewSite/static/GameFiles/apptrail.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776793532|51085963';
+const CACHE_VERSION = '1776816193|191888932';
 /** @type {string} */
 const CACHE_PREFIX = 'AppalachianTrail-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
